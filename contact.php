fd35ecc55f46b9285987fa89d7ef0d90c2ca97d6
Prateek kanhere <prateek.kanhere@goscale.co>
2023-01-20 10:34:47 +0530

    contact form

--- contact.php
+++ contact.php
@@ -1,22 +1,22 @@
 <?php
-
-	$form_data = [];
-
 	$name        	= $_POST['name'];
 	$email        	= $_POST['email'];
-	$cell        	= $_POST['cell'];
+	$phone        	= $_POST['phone'];
 	$city        	= $_POST['city'];
-	$program    	= $_POST['program'];
+	$message    	= $_POST['message'];
 
 
         $email_from = 'Email from Website';
 
         $email_subject = 'New Form Submission';
-        $email_body = "Client Name: $name \n Email: $email, \nCell: $cell, \nCity: $city, \nProgram: $program.";
+        $email_body = "Client Name: $name \n"."
+         Email: $email, \n"."
+         Cell: $phone, \n"."
+         City: $city, \n"."
+         message: $message.\n";
 
         $to = 'alpsinterior5003@gmail.com';
         $headers = "From :   $email_from \r\n";
-
         $headers = "Reply to :   $email \r\n";
         mail($to,$email_subject,$email_body,$headers);
         header("Location: contact.html");
